06726d741ec87a83a30359c7ae29cddec21527ed
yoyzhou <iamyoyozhou@gmail.com>
2013-04-08 00:11:02 +0800

    restructured weibo scrapy and weibo following network scrapy

---
+++ example.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+#coding=utf8
+
+from weibo_scrapy import scrapy
+
+
+class my_scrapy(scrapy):
+    
+    def scrapy_do_task(self, uid=None):
+         '''
+        User needs to implements this method to perform scrapy task, which based on weibo uid.
+        @param uid: weibo uid
+        @return: a list of uids gained from this task
+        '''
+         super(my_scrapy, self).__init__(**kwds)
+         #do what you want with uid here, note that this scrapy is uid based, so make sure there are uids in task queue, 
+         #or gain new uids from this function
+         
+         return 'replace this string with uid list which gained from this task'
+     
+if __name__ == '__main__':
+    
+    s = my_scrapy(start_uid = '1248521225')
+    s.scrapy()
+    
