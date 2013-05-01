3b6c2196a6d271672d8ca69248cce8d85fd01828
yoyzhou <iamyoyozhou@gmail.com>
2013-05-01 18:59:21 +0800

    modified .gitignore

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -12,7 +12,7 @@ from os.path import join
 
 def concatenateAll(following_files_folder):
     '''
-    Concatenate all weibo info resided in following files, in which each line presents a following user.
+    Concatenate all weibo user info resided in following files, in which each line presents a following user.
     Function needs to eliminate duplications
     '''
     following_user_all = open('following_user_all.txt', 'w')
@@ -38,6 +38,47 @@ def concatenateAll(following_files_folder):
     following_user_all.flush()
     following_user_all.close()
 
+def user_info_to_csv(user_info_file):
+    
+    users_needed = set()
+    with open('fntk.csv', 'r') as fntk:
+        for line in fntk:
+            uids = line.split('\t')
+            if not uids[0] in users_needed:
+                users_needed.add(uids[0])
+            if not uids[1] in users_needed:
+                users_needed.add(uids[1])
+                
+    user_info_csv = open('following_user_needed.csv', 'w')
+    
+    user_info_csv.write('Id\tLabel\tSex\n' )
+    with open(user_info_file) as userinfos:
+        for line in userinfos:
+             info_dict = {}
+             if line.strip():
+                 try:
+                     infos = line.split('*_*')
+                     for info in infos:
+                        field = info.split(':')[0]
+                        value = info.split(':')[1]
+                        info_dict[field.strip()] = value.strip()
+                 except:
+                        print line + following_file
+                        pass
+             str_info = ''
+#             str_info += info_dict['uid'] + '\t' + info_dict['sex'] + '\t' + \
+#                               info_dict['fnick'] + '\t' + info_dict['name'] + '\t' +\
+#                               info_dict['face'] + '\t' + info_dict['address'] + '\t' +\
+#                               info_dict['connect'] + '\n'
+             if info_dict['uid'] in users_needed:
+                 str_info += info_dict['uid'] + '\t'  + info_dict['name'] + '\t' + info_dict['sex']  + '\n'
+                 user_info_csv.write(str_info )
+            
+             
+        user_info_csv.flush()
+        user_info_csv.close()
+        
+        
 def aggregate_following_ntk(following_files_folder):
     '''
     Aggregate user following network from all the following files, note each file contains all the followings of
@@ -66,5 +107,7 @@ def aggregate_following_ntk(following_files_folder):
     following_ntk.close()
     
 if __name__ == '__main__':
-    concatenateAll('D:\\dataset\\weibo\\data\\followings')
-    aggregate_following_ntk('D:\\dataset\\weibo\\data\\followings')
+    #concatenateAll('D:\\dataset\\weibo\\data\\followings')
+    #aggregate_following_ntk('D:\\dataset\\weibo\\data\\followings')
+    
+    user_info_to_csv('following_user_all.txt')
